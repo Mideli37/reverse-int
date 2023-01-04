cf4536856816f8ddc75e377a521655858c983522
Mideli <muzipovanastia@gmail.com>
2023-01-04 23:21:13 +0200

    init: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+  (n < 0) ? n = -n : n = n
+  let number = String(n)
+  let i = number.length - 1
+  let num = ''
+  while (i >= 0) {
+    num += number[i]
+    i --
+  }
   
+  return num
 }
